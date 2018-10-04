a8373bfb3d74004d42c4d73bd6c39dfe8e04128c
ggomez1973 <g.gomez@globallogic.com>
2018-10-04 15:43:50 -0300

    Initial commit

---
+++ flights/src/main/java/com/ggomez/ms/flights/FlightsApplication.java
@@ -0,0 +1,12 @@
+package com.ggomez.ms.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsApplication.class, args);
+	}
+}
